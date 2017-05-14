0e172a78383b0416f7d1888f7df2764f1e164ce3
parede_h <parede_h@1dba07ec-cf4d-42dc-a774-cbb40475cd26>
2017-05-14 21:36:07 +0000

    version 3 with name

--- Battle For Midgard/aizpur_v/introduction/main.c
+++ Battle For Midgard/aizpur_v/introduction/main.c
@@ -5,21 +5,28 @@
 ** Login   <aizpur_v@etna-alternance.net>
 ** 
 ** Started on  Wed May 10 10:11:23 2017 AIZPURUA Victor Hugo
-** Last update Sun May 14 20:19:50 2017 PAREDES Alejandra
+** Last update Sun May 14 23:14:13 2017 PAREDES Alejandra
 */
 
 #include <time.h>
 #include <stdlib.h>
 #include "midgar.h"
 
-int		main()
+int		main(int argc, char **argv)
 {
   t_matrix	*matrix;
 
+  if (argc != 3)
+    {
+      my_putstr("Welcome! please put your name after -n \n");
+      my_putstr("E.g: ./sta -n Yoda\n");
+      return (1);
+    }
   srand(time(NULL));
   matrix = create_matrix();
   create_team(matrix);
   create_player(matrix);
+  matrix->player->name = argv[2];
   matrix->creature = get_creature();
   while (matrix->quit != 1)
     {
